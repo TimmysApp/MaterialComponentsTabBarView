4ca9d9ff387f4003aa01bcc48795207645b19612
Joe Maghzal <joe.maghzal@totersapp.com>
2023-11-16 15:17:15 +0200

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -1,23 +1,16 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.7
 
 import PackageDescription
 
 let package = Package(
     name: "MaterialComponentsTabBarView",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "MaterialComponentsTabBarView",
-            targets: ["MaterialComponentsTabBarView"]),
+        .library(name: "MaterialComponentsTabBarView", targets: ["MaterialComponentsTabBarView"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MaterialComponentsTabBarView"),
-        .testTarget(
-            name: "MaterialComponentsTabBarViewTests",
-            dependencies: ["MaterialComponentsTabBarView"]),
+        .target(name: "MaterialComponentsTabBarView", dependencies: [], path: "Source", publicHeadersPath: "include", cSettings: [.headerSearchPath(".")])
     ]
 )
